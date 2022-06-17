84a847c3b05986dda772f5e5e72384dbe6508859
root <root@DESKTOP-VB97GVI.localdomain>
2022-06-17 01:49:32 +0100

    done

--- monty.h
+++ monty.h
@@ -1,11 +1,17 @@
-#ifndef MONTY
-#define MONTY
+#ifndef MONTY_H_
+#define MONTY_H_
+
+#define UN __attribute__((__unused__))
+
+/* Standard Libraries */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <unistd.h>
 
+#include <string.h>
+
+/* Structs and Lists */
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -38,56 +44,59 @@ typedef struct instruction_s
 } instruction_t;
 
 /**
- * struct content  - opcode and its function
- * @op_func: the opcode
- * @value: function to handle the opcode
- * @line_n: number of the line
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * struct list_s - linked list of struct instruction
+ * @n: number of line
+ * @inst: instruction of opcode and its function
+ * @next: points to the next element of the linked list
+ * Description: Single linked list of the struct instruction
  */
-typedef struct content
+typedef struct list_s
 {
-	char *op_func;
-	char *value;
-	int line_n;
-} content_v;
+	int n;
+	char *inst;
+	struct list_s *next;
+} list_t;
 
-/**
- * struct stakit  - opcode and its function
- * @int_value: idk.
- * @data: idk.
- * @fp: idk.
- * @line: idk.
- * Description: honestly, idk.
- */
-typedef struct stakit
-{
-	int int_value;
-	content_v data;
-	FILE *fp;
-	char *line;
-} extern_var_t;
-extern extern_var_t ex;
+extern list_t *list_opcode;
 
-int main(int argc, char *argv[]);
-void read_file(FILE *fp);
-void execute_function(stack_t **stack);
-size_t not_empty(char *line);
-content_v clean_spaces(char *line);
+/* Prototype Functions */
 
-void monty_push(stack_t **top, unsigned int line_n);
-void monty_pall(stack_t **top, unsigned int line_n);
-void monty_pint(stack_t **top, unsigned int line_n);
-void monty_pop(stack_t **top, unsigned int line_n);
-void monty_swap(stack_t **top, unsigned int line_n);
-void monty_add(stack_t **top, unsigned int line_n);
-void monty_sub(stack_t **top_ptr, unsigned int line_n);
-void monty_mul(stack_t **top_ptr, unsigned int line_n);
-void monty_nop(stack_t **top_ptr, unsigned int line_n);
-void monty_div(stack_t **top_ptr, unsigned int line_n);
-void monty_mod(stack_t **top_ptr, unsigned int line_n);
-void monty_pchar(stack_t **top_ptr, unsigned int line_n);
-void monty_pstr(stack_t **top_ptr, unsigned int line_n);
+int get_file(char *str);
+void check_file(char *str);
+list_t *create_instruction(list_t **head, char *str, int n, FILE *fp);
+void (*ptr_opcode(list_t *head))(stack_t **stack, unsigned int line_number);
+char *_strdup(char *str, FILE *fp);
+int _strlen(char *str);
+int clean_string(char *buff, char *str);
+void free_list_opcode(list_t *head);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void nothing(stack_t **stack, unsigned int line_number);
+int cmp_inst(char *str1, char *str);
+stack_t *add_nodeint(stack_t **head, const int n);
+void free_all(list_t *list_opcode, stack_t *stack);
+void free_list_stack(stack_t *head);
+void swap(stack_t **stack, UN unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+int stack_len(stack_t *head);
+void pop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(UN stack_t **stack, UN unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+int check_arg(char *buff);
+int cpy_arg(list_t *t, char *buff);
+void print_error_push(stack_t **stack, int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+void core_stack_queue(stack_t **stack, unsigned int line_number, int sq);
+void queue(stack_t **stack, unsigned int line_number);
+void stack(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, UN unsigned int line_number);
+void rotl(stack_t **stack, UN unsigned int line_number);
+void rotr(stack_t **stack, UN unsigned int line_number);
 
-void free_stack(stack_t *top_ptr);
-#endif
+#endif /* MONTY_H_ */
